fd510f134b3e38c081da7ded35497a9f69ef0e7e
Eric Fields <eric.fields@bc.edu>
2019-04-02 12:26:02 -0400

    update cluster EEGLAB path

--- MUSim_RealERP_Simulations.m
+++ MUSim_RealERP_Simulations.m
@@ -1,7 +1,7 @@
 %Run stats simulations with real EEG noise trials and effects
 %
 %Author: Eric Fields
-%Version Date: 22 March 2019
+%Version Date: 2 April 2019
 
 clearvars; close all;
 
@@ -9,9 +9,7 @@ main_dir = MUSim_main_dir();
 
 %Add EEGLAB, MUT, and FMUT to path if on cluster
 if isunix()
-    addpath('/gsfs0/data/fields/Documents/MATLAB/eeglab14_1_1b_ECF'); %NEED TO CHANGE
-    addpath('/gsfs0/data/fields/Documents/MATLAB/FMUT_0.3.5');
-    addpath('/gsfs0/data/fields/Documents/MATLAB/dmgroppe-Mass_Univariate_ERP_Toolbox-10dc5c7');
+    addpath('/gsfs0/data/fields/Documents/MATLAB/eeglab14_1_2b_ECF');
 end
 
 n_exp  = 5e3;
@@ -20,7 +18,7 @@ save_results = false;
 
 effect = fullfile(main_dir, 'data', 'NonCon_N400_restricted.mat');
 factor_levels = 2;
-time_wind = [0, 1000]; %[300, 500];
+time_wind = [0, 1000];
 electrodes = 1:32; %[10, 14, 21, 22, 23];
 mult_comp_method = 'clust';
 
