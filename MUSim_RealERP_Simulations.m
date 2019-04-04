abfd3edc18c12d679d4590ab765cc0b783177b1f
Eric Fields <eric.fields@bc.edu>
2019-04-04 17:19:10 -0400

    finish all methods generalization and clean up code

--- MUSim_RealERP_Simulations.m
+++ MUSim_RealERP_Simulations.m
@@ -12,17 +12,15 @@ if isunix()
     addpath('/gsfs0/data/fields/Documents/MATLAB/eeglab14_1_2b_ECF');
 end
 
-n_exp  = 1e2;
+n_exp  = 1e3;
 n_perm = 1e3;
+n_trials = 20;
 alpha = 0.05;
 save_results = true;
 
-effect = fullfile(main_dir, 'data', 'NonCon_N400_restricted.mat');
-factor_levels = 2;
-time_wind = [0, 1000];
-electrodes = 1:32; %[10, 14, 21, 22, 23];
-mult_comp_method = 'clust05';
+effect = 'null'; %fullfile(main_dir, 'data', 'namesP300_reduced.mat');
+factor_levels = 3;
+time_wind = [500 750];
+electrodes = [10, 14, 21, 22, 23];
 
-diary(fullfile(main_dir, 'results', 'MUSim_sirius_test.txt'));
-run_real_erp_sim(effect, time_wind, electrodes, factor_levels, n_exp, n_perm, alpha, save_results);
-diary off;
+run_real_erp_sim(effect, time_wind, electrodes, factor_levels, n_exp, n_perm, n_trials, alpha, save_results);
--- run_real_erp_sim.m
+++ run_real_erp_sim.m
@@ -4,7 +4,7 @@
 %Author: Eric Fields
 %Version Date: 4 April 2019
 
-function run_real_erp_sim(effect, time_wind, electrodes, factor_levels, n_exp, n_perm, alpha, save_results)
+function run_real_erp_sim(effect, time_wind, electrodes, factor_levels, n_exp, n_perm, n_trials, alpha, save_results)
 
     
     %% ####################################################################
@@ -13,10 +13,9 @@ function run_real_erp_sim(effect, time_wind, electrodes, factor_levels, n_exp, n
     
     main_dir = MUSim_main_dir();
     
+    %Start EEGLAB
     [ALLEEG, EEG, CURRENTSET, ALLCOM] = eeglab; %#ok<ASGLU>
     close all;
-    rmpath('C:\Users\ecfne\Documents\MATLAB\eeglab14_1_2b_ECF\plugins\FMUT_0.4.1');
-    addpath('C:\Users\ecfne\Documents\Eric\Coding\FMUT_development\FMUT');
     
     %% ~~~~~ SIMULATION PARAMETERS ~~~~~
     
@@ -24,35 +23,30 @@ function run_real_erp_sim(effect, time_wind, electrodes, factor_levels, n_exp, n
     VERBLEVEL = 0;
     
     %Load template GND for time point and electrode information
-    load(fullfile(main_dir, 'data', 'MUSim_template.GND'), '-mat');
+    load(fullfile(main_dir, 'data', 'MUSim_template.GND'), '-mat', 'GND');
     %Load EEG noise trials
-    load(fullfile(main_dir, 'data', 'AXCPT_noise_trials_128Hz_30Hz_49subs.mat'));
+    load(fullfile(main_dir, 'data', 'AXCPT_noise_trials_128Hz_30Hz_49subs.mat'), 'all_noise_trials');
     %Load effect data
-    if effect
-        load(effect);
-        if ~isequal(factor_levels, size(effects_data, 3)) %#ok<NODEF>
+    if exist(effect, 'file') && ~strcmpi(effect, 'null')
+        load(effect, 'effects_data', 'effect_description');
+        if ~isequal(factor_levels, size(effects_data, 3))
             error('factor_levels input doesn''t match effects data');
         end
+    else
+        effect = false;
     end
     
     %Simulated data characteristics
     n_subs = 24;
     n_time_pts = length(GND.time_pts);
     bins = 1:prod(factor_levels);
-    error_mult  = ones(length(bins), 1) * 3;
-    cond_trials = ones(length(bins), 1) * 20;
-    sub_param = [1 0.1];
+    error_mult  = ones(length(bins), 1); %Multiplier applied to standard deviation of each bin
+    cond_trials = ones(length(bins), 1) * n_trials; %Number of trials in each bin
+    ind_var_factor = 0.1; %SD of normal distribuiton for individual difference multiplier
     
-    %Parameters for permutation ANOVA
+    %Channel neighbor information for cluster tests
     chan_hood = 75;
     chan_hood = spatial_neighbors(GND.chanlocs(electrodes), chan_hood, []);
-    thresh_p = 0.05;
-    
-    %Parameters for parametric ANOVA
-    param_time_wind  = time_wind;
-    param_electrodes = electrodes;
-    [~, param_start_sample] = min(abs(GND.time_pts - param_time_wind(1)));
-    [~, param_end_sample  ] = min(abs(GND.time_pts - param_time_wind(2)));
     
     %Other useful numbers
     n_electrodes = length(electrodes);
@@ -81,17 +75,11 @@ function run_real_erp_sim(effect, time_wind, electrodes, factor_levels, n_exp, n
     h_bky     = NaN(n_exp, n_electrodes, n_sample_time_pts);
     h_mean_amp = NaN(n_exp, 1);
     p_uncorr  = NaN(n_exp, n_electrodes, n_sample_time_pts);
-    p_Fmax    = NaN(n_exp, n_electrodes, n_sample_time_pts);
-    p_clust05 = NaN(n_exp, n_electrodes, n_sample_time_pts);
-    p_clust01 = NaN(n_exp, n_electrodes, n_sample_time_pts);
-    p_bh      = NaN(n_exp, n_electrodes, n_sample_time_pts);
-    p_by      = NaN(n_exp, n_electrodes, n_sample_time_pts);
-    p_mean_amp = NaN(n_exp, 1);
 
     %Randomly select subjects to use in each experiment below
     sub_sample = NaN(n_exp, n_subs);
     for i = 1:n_exp
-        sub_sample(i, :) = randsample(1:size(all_noise_trials, 1), n_subs); %#ok<USENS>
+        sub_sample(i, :) = randsample(1:size(all_noise_trials, 1), n_subs);
     end
     
     %Load effects data
@@ -112,16 +100,17 @@ function run_real_erp_sim(effect, time_wind, electrodes, factor_levels, n_exp, n
         %Randomly select trials from each subject
         sim_data = NaN(n_electrodes, n_time_pts, n_conds, n_subs);
         for s = 1:n_subs
-            sim_data(:,:,:,s) = mean(reshape(all_noise_trials{sub_sample(i,s),2}(electrodes, :, randsample(1:size(all_noise_trials{sub_sample(i,s),2},3), n_trials)), n_electrodes, n_time_pts, n_conds, []), 4);
+            sub_data = all_noise_trials{sub_sample(i,s),2}; %#ok<PFBNS,PFIIN>
+            sim_data(:,:,:,s) = mean(reshape(sub_data(electrodes, :, randsample(1:size(sub_data,3), n_trials)), n_electrodes, n_time_pts, n_conds, []), 4);
         end
         
         %Add effects if any
         if effect
             %Generate subject effects for this experiment
-            sub_effects = normrnd(sub_param(1), sub_param(2), n_subs, n_conds);
+            sub_effects = normrnd(1, ind_var_factor, n_subs, n_conds);
             for s = 1:n_subs
                 for b = 1:n_conds
-                    sim_data(:, :, b, s) = sim_data(:, :, b, s)*error_mult(b) + effects_data(:, :, b)*sub_effects(s, b);
+                    sim_data(:, :, b, s) = sim_data(:, :, b, s)*error_mult(b) + effects_data(:, :, b)*sub_effects(s, b); %#ok<PFBNS>
                 end
             end
         end
@@ -136,37 +125,29 @@ function run_real_erp_sim(effect, time_wind, electrodes, factor_levels, n_exp, n
         [F_obs, F_dist, df_effect, df_res] = perm_rbANOVA(data, 3, n_perm);
         
         %Fmax
-        [h_Fmax(i, :, :), p_Fmax(i, :, :)] = Fmax_corr(F_obs, F_dist, alpha);
+        h_Fmax(i, :, :) = Fmax_corr(F_obs, F_dist, alpha);
         thresh_F = finv(1-0.05, df_effect, df_res);
         
         %cluster
-        [h_clust05(i, :, :), p_clust05(i, :, :)] = Fclust_corr(F_obs, F_dist, alpha, chan_hood, thresh_F);
+        h_clust05(i, :, :) = Fclust_corr(F_obs, F_dist, alpha, chan_hood, thresh_F);
         thresh_F = finv(1-0.01, df_effect, df_res);
-        [h_clust01(i, :, :), p_clust01(i, :, :)] = Fclust_corr(F_obs, F_dist, alpha, chan_hood, thresh_F);
+        h_clust01(i, :, :) = Fclust_corr(F_obs, F_dist, alpha, chan_hood, thresh_F);
         
         %FDR
         p_uncorr(i, :, :) = 1 - fcdf(F_obs, df_effect, df_res);
         h_uncorr(i, :, :) = p_uncorr(i, :, :) <= alpha;
-        [h_bh(i, :, :), ~, ~, p_bh(i, :, :)] = fdr_bh(p_uncorr(i, :, :), alpha, 'pdep', 'no');
-        [h_by(i, :, :), ~, ~, p_by(i, :, :)] = fdr_bh(p_uncorr(i, :, :), alpha, 'dep', 'no');
+        h_bh(i, :, :)  = fdr_bh(p_uncorr(i, :, :), alpha, 'pdep', 'no');
+        h_by(i, :, :)  = fdr_bh(p_uncorr(i, :, :), alpha, 'dep', 'no');
         h_bky(i, :, :) = fdr_bky(p_uncorr(i, :, :), alpha, 'no');
         
         %Mean amplitude ANOVA
-        if isequal(electrodes, 1:32)
-            mean_wind_data = mean(mean(sim_data(param_electrodes, param_start_sample:param_end_sample, bins, :), 1), 2);
-            mean_wind_data = reshape(mean_wind_data, [size(mean_wind_data, 1), size(mean_wind_data, 2), factor_levels, size(mean_wind_data, 4)]);
-        elseif isequal(electrodes, param_electrodes)
-            mean_wind_data = mean(mean(sim_data(:, param_start_sample:param_end_sample, bins, :), 1), 2);
-            mean_wind_data = reshape(mean_wind_data, [size(mean_wind_data, 1), size(mean_wind_data, 2), factor_levels, size(mean_wind_data, 4)]);
-        else
-            error('Can''t determine electrodes for parametric test');
-        end
+        mean_wind_data = mean(mean(data, 1), 2);
+        mean_wind_data = reshape(mean_wind_data, [size(mean_wind_data, 1), size(mean_wind_data, 2), factor_levels, size(mean_wind_data, 4)]);
         param_results = calc_param_ANOVA(mean_wind_data, [], (1:length(factor_levels > 1))+2, alpha);
-        p_mean_amp(i) = param_results.p;
         h_mean_amp(i) = param_results.h;
         
     end
-    toc
+    sim_time = toc;
     
     %Calcluate Dunn-Sidak corrected results
     p_sidak = 1-(1-p_uncorr).^(n_electrodes*n_time_pts);
@@ -186,7 +167,7 @@ function run_real_erp_sim(effect, time_wind, electrodes, factor_levels, n_exp, n
     end
     
     if ~effect
-        effect = 'none';
+        effect = 'none (null)';
         effect_description = 'all effects null';
     end
     
@@ -194,10 +175,11 @@ function run_real_erp_sim(effect, time_wind, electrodes, factor_levels, n_exp, n
     fprintf('\n');
     fprintf('----------------------------------------------------------------------------------\n')
     fprintf('****SIMULATION SUMMARY****\n')
-    fprintf('%s\n\n', time_stamp);
+    fprintf('%s\n', time_stamp);
+    fprintf('\nSimulation took %.2f minutes\n', sim_time/60);
     
-    fprintf('Using real EEG noise trials\n');
-    fprintf('%d simulated experiments, %d permutations each, %d subjects\n\n', n_exp, n_perm, n_subs);
+    fprintf('\nUsing real EEG noise trials\n');
+    fprintf('%d simulated experiments, %d permutations each, %d subjects\n', n_exp, n_perm, n_subs);
     fprintf('\nEffect: %s', effect);
     fprintf('\nEffect description: %s\n', effect_description);
     fprintf('Time window: %d - %d\n', time_wind(1), time_wind(2));
